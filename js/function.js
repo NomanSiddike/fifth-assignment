b2f3491357127fe4f7247698b09bfb458f2ec5ed
NomanSiddike <nomansiddike0048@gmail.com>
2024-09-23 13:43:35 +0600

    three donation card functionality partially completed

---
+++ js/function.js
@@ -0,0 +1,18 @@
+function getInputFieldElementById(id) {
+  return document.getElementById(id);
+}
+
+function getInputFieldById(id) {
+  return parseFloat(document.getElementById(id).value);
+}
+
+function getTextElementById(id) {
+  return document.getElementById(id);
+}
+function getTextById(id) {
+  return parseFloat(document.getElementById(id).innerText);
+}
+
+function subtraction(num1, num2) {
+  return num1 - num2;
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,65 @@
+//  Donor Card1 Activities
+document
+  .getElementById("donation-card1-button")
+  .addEventListener("click", function () {
+    const donationCard1InputAmount = getInputFieldById(
+      "donation-Card1-Input-Amount"
+    );
+    const card1DonatedAmount = getTextById("card1-donated-amount");
+    const card1DonatedAmountElement = getTextElementById(
+      "card1-donated-amount"
+    );
+    const donarBalanceElement = getTextElementById("donar-balance"); // dom element
+    const donarBalance = getTextById("donar-balance"); //inner text
+    const remainingBalance = subtraction(
+      donarBalance,
+      donationCard1InputAmount
+    );
+    donarBalanceElement.innerText = remainingBalance;
+    card1DonatedAmountElement.innerText =
+      card1DonatedAmount + donationCard1InputAmount;
+  });
 
+// Donar Card2 Activities
+document
+  .getElementById("donation-card2-button")
+  .addEventListener("click", function () {
+    const donationCard2InputAmount = getInputFieldById(
+      "donation-Card2-Input-Amount"
+    );
+    const card2DonatedAmount = getTextById("card2-donated-amount");
+    const card2DonatedAmountElement = getTextElementById(
+      "card2-donated-amount"
+    );
+    const donarBalanceElement = getTextElementById("donar-balance"); // dom element
+    const donarBalance = getTextById("donar-balance"); //inner text
+    const remainingBalance = subtraction(
+      donarBalance,
+      donationCard2InputAmount
+    );
+    donarBalanceElement.innerText = remainingBalance;
+    card2DonatedAmountElement.innerText =
+      card2DonatedAmount + donationCard2InputAmount;
+  });
+
+// Donar Card3 Activities
+document
+  .getElementById("donation-card3-button")
+  .addEventListener("click", function () {
+    const donationCard3InputAmount = getInputFieldById(
+      "donation-Card3-Input-Amount"
+    );
+    const card3DonatedAmount = getTextById("card3-donated-amount");
+    const card3DonatedAmountElement = getTextElementById(
+      "card3-donated-amount"
+    );
+    const donarBalanceElement = getTextElementById("donar-balance"); // dom element
+    const donarBalance = getTextById("donar-balance"); //inner text
+    const remainingBalance = subtraction(
+      donarBalance,
+      donationCard3InputAmount
+    );
+    donarBalanceElement.innerText = remainingBalance;
+    card3DonatedAmountElement.innerText =
+      card3DonatedAmount + donationCard3InputAmount;
+  });
